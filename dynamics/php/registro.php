8e5be022f2c1dd49dd18941a7ef3c2a3b07cb2f4
Sof Camacho <321199467@alumno.enp.unam.mx>
2022-06-01 01:15:34 -0500

    feat (): redireccionamientos de las 3 primeras vistas

--- dynamics/php/registro.php
+++ dynamics/php/registro.php
@@ -16,31 +16,43 @@
                     <input type='text' id='nombre' name='nombre' placeholder='Nombre' required>
                     <input type='text' id='apellido' name='apellido' placeholder='Apellido' required>
                     <br/>
-                    <input type='text' id='contrasena' name='contrasena' placeholder='Contraseña' required>
-                    <input type='text' id='contrasenaConfir' name='contrasenaConfir' placeholder='Confirmar Contraseña' required>
+                    <input type='password' id='contrasena' name='contrasena' placeholder='Contraseña' required>
+                    <input type='password' id='contrasenaConfir' name='contrasenaConfir' placeholder='Confirmar Contraseña' required>
                     <br/>
                     <input type='email' id='correo' name='correo' placeholder='Correo Electrónico' required>
                     <input type='email' id='correoConfir' name='correoConfir' placeholder='Confirmar correo Electrónico' required>
                     <br/>
                     <input type='number' id='ntrabajador' name='ntrabajador' placeholder='Número de Trabajador' required>
-                    <input type='number' id='RFC' name='RFC' placeholder='RFC' required>
+                    <input type='text' id='RFC' name='RFC' placeholder='RFC' required>
                     <br/>
                     <input type='number' id='ncuenta' name='ncuenta' placeholder='Numero de Cuenta' required>
                     <br/>
                     <button type='submit'>Crear Cuenta</button>
+                    <br/>
+                    <input type='file' id='imgUsuario'>
+                    <br/>
+                    <label>Agregar Foto<label>
                 </fieldset>
             </form>
         ";
         
         $nombre = (isset($_POST['nombre']) && $_POST["nombre"] != "") ? $_POST['nombre'] : "Falta Nombre";
-        $usuario = (isset($_POST['usuario']) && $_POST["usuario"] != "") ? $_POST['usuario'] : "Falta Usuario";
+        $apellido = (isset($_POST['apellido']) && $_POST["apellido"] != "") ? $_POST['apellido'] : "Falta Apellido";
+        
         $correo = (isset($_POST['correo']) && $_POST["correo"] != "") ? $_POST['correo'] : "Falta Correo";
-        $contra = (isset($_POST['contra']) && $_POST["contra"] != "") ? $_POST['contra'] : "Falta Contraseña";
-        $ncuenta = (isset($_POST['ncuenta']) && $_POST["ncuenta"] != "") ? $_POST['ncuenta'] : "Falta Número de Cuenta";
+        $correoConfir = (isset($_POST['correoConfir']) && $_POST["correoConfir"] != "") ? $_POST['correoConfir'] : "Falta Confirmación de Correo";
 
+        $contrasena = (isset($_POST['contrasena']) && $_POST["contrasena"] != "") ? $_POST['contrasena'] : "Falta Contraseña";
+        $contrasena_confir = (isset($_POST['contrasenaConfir']) && $_POST["contrasenaConfir"] != "") ? $_POST['contrasenaConfir'] : "Falta Confirmación de Contraseña";
+
+        $n_trabajador = (isset($_POST['ntrabajador']) && $_POST["ntrabajador"] != "") ? $_POST['ntrabajador'] : "Falta Número de trabajador";
+        $rfc = (isset($_POST['RFC']) && $_POST["RFC"] != "") ? $_POST['RFC'] : "Falta RFC";
+
+        $ncuenta = (isset($_POST['ncuenta']) && $_POST["ncuenta"] != "") ? $_POST['ncuenta'] : "Falta Número de Cuenta";
+        
         echo "<br/><br/>";
         echo "<h3>Información de usuario</h3>";
-        echo $nombre."<br/>".$usuario."<br/>".$correo."<br/>".$contra."<br/>".$ncuenta;
+        echo $nombre."<br/>".$apellido."<br/>".$correo."<br/>".$contrasena."<br/>".$n_trabajador."<br/>".$rfc."<br/>".$ncuenta;
     
     ?>
 
