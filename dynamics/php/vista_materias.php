ae17cb66e8d3ce56b5f2138b800312371cc4884d
unknown <321129181@alumno.enp.unam.mx.com>
2022-06-01 01:56:06 -0500

    actualizado php general con vista de materias

--- dynamics/php/vista_materias.php
+++ dynamics/php/vista_materias.php
@@ -4,11 +4,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="../../bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Tus materias</title>
 </head>
 <body>
 
-    <img src="../statics/wireframes_2.jpg">
+    <!-- Barra de navegación -->   
+    <div id="barra">
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Menu
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Perfil</a></li>
+                  <li><a class="dropdown-item" href="#">Materias</a></li>
+                  <li><a class="dropdown-item" href="#">Tablero</a></li>
+                  <li><a class="dropdown-item" href="#">Calendario</a></li>
+                  <li><a class="dropdown-item" href="#">Juegos</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <span class="navbar-brand">LISTA DE ASIGNATURAS</span>
+        </div>
+      </nav>
+    </div>
+    <!-- Barra de navegación -->
+    
+    <div id="materias">
+      <!-- aqui dentro se añadirán las materias con un js -->
+    </div>
+    <div id="botones">
+      <button id="id">Agregar materias</button>
+      <button id="id">Eliminar materias</button>
+    </div>
+    
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
