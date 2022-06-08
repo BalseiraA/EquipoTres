1b309b10cf714c089419ddc74d8772f9bc50599f
unknown <321129181@alumno.enp.unam.mx.com>
2022-06-08 02:51:12 -0500

    Actualizada base de datos

--- docs/BaseProyecto.sql
+++ docs/BaseProyecto.sql
@@ -1,6 +1,6 @@
 -- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
 --
--- Host: localhost    Database: EquipoTres
+-- Host: localhost    Database: equipotres
 -- ------------------------------------------------------
 -- Server version	10.4.24-MariaDB
 
@@ -16,6 +16,35 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `eventos`
+--
+
+DROP TABLE IF EXISTS `eventos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `eventos` (
+  `Evento_ID` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `Materia_ID` tinyint(4) NOT NULL,
+  `Usuario_ID` tinyint(4) NOT NULL,
+  `Titulo` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `Fecha` date NOT NULL,
+  `HoraInicial` time DEFAULT NULL,
+  `HoraFinal` time DEFAULT NULL,
+  `Descripcion` varchar(500) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`Evento_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `eventos`
+--
+
+LOCK TABLES `eventos` WRITE;
+/*!40000 ALTER TABLE `eventos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `eventos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `grupo`
 --
 
@@ -23,8 +52,7 @@ DROP TABLE IF EXISTS `grupo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `grupo` (
-  `Grupo_ID` tinyint(4) NOT NULL,
-  `Grupo` varchar(10) DEFAULT NULL,
+  `Grupo_ID` int(11) NOT NULL,
   PRIMARY KEY (`Grupo_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -35,10 +63,7 @@ CREATE TABLE `grupo` (
 
 LOCK TABLES `grupo` WRITE;
 /*!40000 ALTER TABLE `grupo` DISABLE KEYS */;
-
-INSERT INTO `equipotres`.`grupo` (Grupo_ID, Grupo) VALUES (1, '560');
-INSERT INTO `equipotres`.`grupo` (Grupo_ID, Grupo) VALUES (2, '561');
-
+INSERT INTO `grupo` VALUES (401),(402),(403),(404),(405),(406),(407),(408),(409),(410),(411),(412),(413),(414),(415),(416),(417),(451),(452),(453),(454),(455),(456),(457),(458),(459),(460),(461),(462),(463),(464),(465),(466),(501),(502),(503),(504),(505),(506),(507),(508),(509),(510),(511),(512),(513),(514),(515),(516),(517),(518),(551),(552),(553),(554),(555),(556),(557),(558),(559),(560),(561),(562),(563),(564),(601),(602),(603),(604),(605),(606),(607),(608),(609),(610),(611),(612),(613),(614),(615),(616),(617),(618),(619),(651),(652),(653),(654),(655),(656),(657),(658),(659),(660),(661),(662),(663),(664);
 /*!40000 ALTER TABLE `grupo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,14 +75,11 @@ DROP TABLE IF EXISTS `materiahasgrupo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `materiahasgrupo` (
+  `MateriaHasGrupo_ID` tinyint(4) DEFAULT NULL,
   `Materia_ID` tinyint(4) DEFAULT NULL,
-  `Grupo_ID` tinyint(4) DEFAULT NULL,
-  `MateriaHasGrupo_ID` tinyint(4) NOT NULL,
-  PRIMARY KEY (`MateriaHasGrupo_ID`),
-  KEY `Materia_ID` (`Materia_ID`),
+  `Grupo_ID` int(11) DEFAULT NULL,
   KEY `Grupo_ID` (`Grupo_ID`),
-  CONSTRAINT `materiahasgrupo_ibfk_1` FOREIGN KEY (`Materia_ID`) REFERENCES `materias` (`Materia_ID`),
-  CONSTRAINT `materiahasgrupo_ibfk_2` FOREIGN KEY (`Grupo_ID`) REFERENCES `grupo` (`Grupo_ID`)
+  CONSTRAINT `materiahasgrupo_ibfk_1` FOREIGN KEY (`Grupo_ID`) REFERENCES `grupo` (`Grupo_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -67,14 +89,6 @@ CREATE TABLE `materiahasgrupo` (
 
 LOCK TABLES `materiahasgrupo` WRITE;
 /*!40000 ALTER TABLE `materiahasgrupo` DISABLE KEYS */;
-
-INSERT INTO `equipotres`.`materiahasgrupo` (MateriaHasGrupo_ID, Materia_ID, Grupo_ID) VALUES (1, 1, 1);
-INSERT INTO `equipotres`.`materiahasgrupo` (MateriaHasGrupo_ID, Materia_ID, Grupo_ID) VALUES (2, 2, 1);
-INSERT INTO `equipotres`.`materiahasgrupo` (MateriaHasGrupo_ID, Materia_ID, Grupo_ID) VALUES (2, 3, 1);
-INSERT INTO `equipotres`.`materiahasgrupo` (MateriaHasGrupo_ID, Materia_ID, Grupo_ID) VALUES (2, 4, 1);
-INSERT INTO `equipotres`.`materiahasgrupo` (MateriaHasGrupo_ID, Materia_ID, Grupo_ID) VALUES (2, 5, 1);
-INSERT INTO `equipotres`.`materiahasgrupo` (MateriaHasGrupo_ID, Materia_ID, Grupo_ID) VALUES (2, 6, 1);
-
 /*!40000 ALTER TABLE `materiahasgrupo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -98,15 +112,7 @@ CREATE TABLE `materias` (
 
 LOCK TABLES `materias` WRITE;
 /*!40000 ALTER TABLE `materias` DISABLE KEYS */;
-
-INSERT INTO `equipotres`.`materias` (Materia_ID, Materia) VALUES (0, 'Todas');
-INSERT INTO `equipotres`.`materias` (Materia_ID, Materia) VALUES (1, 'Matemáticas');
-INSERT INTO `equipotres`.`materias` (Materia_ID, Materia) VALUES (2, 'Literatura');
-INSERT INTO `equipotres`.`materias` (Materia_ID, Materia) VALUES (3, 'Física');
-INSERT INTO `equipotres`.`materias` (Materia_ID, Materia) VALUES (4, 'Química');
-INSERT INTO `equipotres`.`materias` (Materia_ID, Materia) VALUES (5, 'Biología');
-INSERT INTO `equipotres`.`materias` (Materia_ID, Materia) VALUES (6, 'Historia');
-
+INSERT INTO `materias` VALUES (0,'Todas'),(1,'Matemáticas'),(2,'Literatura'),(3,'Física'),(4,'Química'),(5,'Biología'),(6,'Historia');
 /*!40000 ALTER TABLE `materias` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -118,13 +124,13 @@ DROP TABLE IF EXISTS `turno`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `turno` (
-  `Turno_ID` tinyint(4) NOT NULL,
-  `Usuario_ID` tinyint(4) DEFAULT NULL,
-  `TipoTurno` varchar(15) DEFAULT NULL,
+  `Turno_ID` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `Grupo_ID` int(11) DEFAULT NULL,
+  `TipoTurno` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`Turno_ID`),
-  KEY `Usuario_ID` (`Usuario_ID`),
-  CONSTRAINT `turno_ibfk_1` FOREIGN KEY (`Usuario_ID`) REFERENCES `usuario` (`Usuario_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `Grupo_ID` (`Grupo_ID`),
+  CONSTRAINT `turno_ibfk_1` FOREIGN KEY (`Grupo_ID`) REFERENCES `grupo` (`Grupo_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -133,10 +139,7 @@ CREATE TABLE `turno` (
 
 LOCK TABLES `turno` WRITE;
 /*!40000 ALTER TABLE `turno` DISABLE KEYS */;
-
-INSERT INTO `equipotres`.`turno` (Turno_ID, Usuario_ID, TipoTurno) VALUES (1, 1, 'Vespertino');
-INSERT INTO `equipotres`.`turno` (Turno_ID, Usuario_ID, TipoTurno) VALUES (2, 2, 'Matutino');
-
+INSERT INTO `turno` VALUES (1,401,'matutino'),(2,402,'matutino'),(3,403,'matutino'),(4,404,'matutino'),(5,405,'matutino'),(6,406,'matutino'),(7,407,'matutino'),(8,408,'matutino'),(9,409,'matutino'),(10,410,'matutino'),(11,411,'matutino'),(12,412,'matutino'),(13,413,'matutino'),(14,414,'matutino'),(15,415,'matutino'),(16,416,'matutino'),(17,417,'matutino'),(18,451,'vespertino'),(19,452,'vespertino'),(20,453,'vespertino'),(21,454,'vespertino'),(22,455,'vespertino'),(23,456,'vespertino'),(24,457,'vespertino'),(25,458,'vespertino'),(26,459,'vespertino'),(27,460,'vespertino'),(28,461,'vespertino'),(29,462,'vespertino'),(30,463,'vespertino'),(31,464,'vespertino'),(32,465,'vespertino'),(33,501,'matutino'),(34,502,'matutino'),(35,503,'matutino'),(36,504,'matutino'),(37,505,'matutino'),(38,506,'matutino'),(39,507,'matutino'),(40,508,'matutino'),(41,509,'matutino'),(42,510,'matutino'),(43,511,'matutino'),(44,512,'matutino'),(45,513,'matutino'),(46,514,'matutino'),(47,515,'matutino'),(48,516,'matutino'),(49,517,'matutino'),(50,518,'matutino'),(51,551,'vespertino'),(52,552,'vespertino'),(53,553,'vespertino'),(54,554,'vespertino'),(55,555,'vespertino'),(56,556,'vespertino'),(57,557,'vespertino'),(58,558,'vespertino'),(59,559,'vespertino'),(60,560,'vespertino'),(61,561,'vespertino'),(62,562,'vespertino'),(63,563,'vespertino'),(64,564,'vespertino'),(66,601,'matutino'),(67,602,'matutino'),(68,603,'matutino'),(69,604,'matutino'),(70,605,'matutino'),(71,606,'matutino'),(72,607,'matutino'),(73,608,'matutino'),(74,609,'matutino'),(75,610,'matutino'),(76,611,'matutino'),(77,612,'matutino'),(78,613,'matutino'),(79,614,'matutino'),(80,615,'matutino'),(81,616,'matutino'),(82,617,'matutino'),(83,618,'matutino'),(84,619,'matutino'),(86,651,'vespertino'),(87,652,'vespertino'),(88,653,'vespertino'),(89,654,'vespertino'),(90,655,'vespertino'),(91,656,'vespertino'),(92,657,'vespertino'),(93,658,'vespertino'),(94,659,'vespertino'),(95,660,'vespertino'),(96,661,'vespertino'),(97,662,'vespertino'),(98,663,'vespertino'),(99,664,'vespertino');
 /*!40000 ALTER TABLE `turno` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -148,18 +151,18 @@ DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
-  `Usuario_ID` tinyint(4) NOT NULL,
-  `Grupo_ID` tinyint(4) DEFAULT NULL,
+  `Usuario_ID` varchar(20) NOT NULL,
+  `Turno_ID` tinyint(4) DEFAULT NULL,
   `Nombre` varchar(20) DEFAULT NULL,
-  `Apellidos` varchar(20) DEFAULT NULL,
-  `Correo` varchar(50) DEFAULT NULL,
+  `Apellidos` varchar(30) DEFAULT NULL,
+  `Correo` varchar(30) DEFAULT NULL,
   `Contrasena` varchar(20) DEFAULT NULL,
-  `RFC` varchar(15) DEFAULT NULL,
-  `NumCuenta` varchar(10) DEFAULT NULL,
-  `NumTrabajador` varchar(15) DEFAULT NULL,
+  `RFC` varchar(20) DEFAULT NULL,
+  `NumCuenta` varchar(20) DEFAULT NULL,
+  `NumTrabajor` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`Usuario_ID`),
-  KEY `Grupo_ID` (`Grupo_ID`),
-  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`Grupo_ID`) REFERENCES `grupo` (`Grupo_ID`)
+  KEY `Turno_ID` (`Turno_ID`),
+  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`Turno_ID`) REFERENCES `turno` (`Turno_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -169,48 +172,9 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-
-INSERT INTO `equipotres`.`usuario`
-(Usuario_ID, Grupo_ID, Nombre, Apellidos, Correo, Contrasena, RFC, NumCuenta, NumTrabajador)
-VALUES
-(1, 0, 'Estefanni', 'Torres', 'efanni@hotmail.com', '123123', 'E01082007G23', NULL, '00001');
-INSERT INTO `equipotres`.`usuario`
-(Usuario_ID, Grupo_ID, Nombre, Apellidos, Correo, Contrasena, RFC, NumCuenta, NumTrabajador)
-VALUES
-(1, 0, 'Jennifer', 'Vazquez', 'jenny@gmail.com', '123123', NULL, '321056553', NULL);
-
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
---
--- Table structure for table `eventos`
---
-
-DROP TABLE IF EXISTS `eventos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `eventos` (
-  `Evento_ID` tinyint(4) NOT NULL AUTO_INCREMENT,
-  `Materia_ID` tinyint(4) NOT NULL,
-  `Usuario_ID` tinyint(4) NOT NULL,
-  `Titulo` nvarchar(100) NOT NULL,
-  `Fecha` date NOT NULL,
-  `HoraInicial` time NULL,
-  `HoraFinal` time NULL,
-  `Descripcion` nvarchar(500) NOT NULL,
-  PRIMARY KEY (`Evento_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `eventos`
---
-
-LOCK TABLES `eventos` WRITE;
-/*!40000 ALTER TABLE `eventos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `eventos` ENABLE KEYS */;
-UNLOCK TABLES;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -220,4 +184,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-06  13:03:56
+-- Dump completed on 2022-06-08  2:49:46
