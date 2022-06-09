a21d2cfdfb13e9c31db53d3a7ea49e7a3a49fd64
unknown <321129181@alumno.enp.unam.mx.com>
2022-06-09 01:28:46 -0500

    Arreglado bug de inicio

--- dynamics/php/inicio.php
+++ dynamics/php/inicio.php
@@ -12,44 +12,43 @@
 
     <h1>Bienvenid@ a CoyoAulas</h1>    
     <form action='' method='post'>
-        <input id='apodo' type='apodo' placeholder='Apodo'>
-        <br/><br/>
-        <input id='contrasena' type='password' placeholder='Contraseña'>
-        <button>Iniciar Sesión</button>
+        <fieldset>
+            <input id='apodo' name='apodo' type='apodo' placeholder='Apodo'>
+            <br/><br/>
+            <input id='contrasena' type='password' placeholder='Contraseña'>
+            <br><br>
+            <button type='submit'>Iniciar Sesión</button>
 
-        <?php
-            $apodo = (isset($_POST["apodo"]) && $_POST["apodo"] !== "")? $_POST["apodo"] : false;
-            
-            if($apodo != ""){
-                include("./config.php");
-                $con = connect();
-            
-                $sql = "SELECT Usuario_ID FROM usuario WHERE Usuario_ID LIKE '$apodo'";
-                $query = mysqli_query($con, $sql);
+            <?php
+                $apodo = (isset($_POST["apodo"]) && $_POST["apodo"] !== "")? $_POST["apodo"] : false;
+                
+                if($apodo != ""){
+                    include("./config.php");
+                    $con = connect();
+                
+                    $sql = "SELECT * FROM usuario WHERE Usuario_ID LIKE '$apodo';";
+                    $query = mysqli_query($con, $sql);
 
-                $row = mysqli_fetch_array($query);
+                    $row = mysqli_fetch_array($query);
 
-                var_dump($row);
-
-                if($row == ""){
-                    echo "
-                    <p>No estás registrado :(</p>";
+                    if($row == ""){
+                        echo "
+                        <p>No estás registrado :(</p>";
+                    }
+                    else {
+                        header("location: ./vista_materias.php");
+                    }
                 }
-                else {
-                    header("location: ./vista_materias.php");
-
-                }
-            }
-        
-        ?>
-
+            
+            ?>
+        </fieldset>
     </form>
 
     <br/>
 
 
     <!-- Formulario Registro  -->
-    <form action='./dynamics/php/intermedio.php' method='post'>
+    <form action='./intermedio.php' method='post'>
         <label>¿Aún no te has registrado?</label>
         <br/><br/>
         <button>Crea una cuenta</button>
